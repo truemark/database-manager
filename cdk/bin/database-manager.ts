670365a9630cd9a17b847baa4f3cfa5a73ed81f2
Lisa Koivu <lisamarie.koivu@gmail.com>
2024-04-11 10:28:33 -0400

    fixed problem in stub with incorrect subnets

--- cdk/bin/database-manager.ts
+++ cdk/bin/database-manager.ts
@@ -9,7 +9,7 @@ import {ExtendedApp} from 'truemark-cdk-lib/aws-cdk';
 
 const databaseManagerStackProps: DatabaseManagerStackProps = {
   vpcId: 'vpc-09186ce91104e20c6', // TODO Populate from your dev account, it's fine to check in, we'll remove it later
-  privateSubnetIds: ['subnet-0ca25478c2d35b8e4', 'subnet-094a6a1daa32e3a52'],
+  privateSubnetIds: ['subnet-00b7b293bc7e39ad5', 'subnet-091ee26f6c54a5b1c'],
   availabilityZones: ['us-west-2a', 'us-west-2b'],
   vpcCidrBlock: '10.200.0.0/20',
   account: process.env.CDK_DEFAULT_ACCOUNT,
