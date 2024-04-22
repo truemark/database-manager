d6e9c503a0fb75267eaa7df74aae09835c519c2d
Lisa Koivu <lisamarie.koivu@gmail.com>
2024-04-21 20:46:59 -0400

    cleanup

--- cdk/bin/database-manager.ts
+++ cdk/bin/database-manager.ts
@@ -8,27 +8,27 @@ import {
 import {ExtendedApp} from 'truemark-cdk-lib/aws-cdk';
 
 // lkoivu truemark account
-const databaseManagerStackProps: DatabaseManagerStackProps = {
-  vpcId: 'vpc-09186ce91104e20c6',
-  privateSubnetIds: ['subnet-00b7b293bc7e39ad5', 'subnet-091ee26f6c54a5b1c'],
-  availabilityZones: ['us-west-2a', 'us-west-2b'],
-  vpcCidrBlock: '10.200.0.0/20',
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
-};
-
 // const databaseManagerStackProps: DatabaseManagerStackProps = {
-//   vpcId: 'vpc-057ded8a4829d01a7',
-//   privateSubnetIds: [
-//     'subnet-01001cca4307389db',
-//     'subnet-0478b2f477f72229c',
-//     'subnet-00d785a8314042ed7',
-//   ],
-//   availabilityZones: ['us-west-2a', 'us-west-2b', 'us-west-2c'],
-//   vpcCidrBlock: '10.238.0.0/19',
+//   vpcId: 'vpc-09186ce91104e20c6',
+//   privateSubnetIds: ['subnet-00b7b293bc7e39ad5', 'subnet-091ee26f6c54a5b1c'],
+//   availabilityZones: ['us-west-2a', 'us-west-2b'],
+//   vpcCidrBlock: '10.200.0.0/20',
 //   account: process.env.CDK_DEFAULT_ACCOUNT,
 //   region: process.env.CDK_DEFAULT_REGION,
 // };
+
+const databaseManagerStackProps: DatabaseManagerStackProps = {
+  vpcId: 'vpc-057ded8a4829d01a7',
+  privateSubnetIds: [
+    'subnet-01001cca4307389db',
+    'subnet-0478b2f477f72229c',
+    'subnet-00d785a8314042ed7',
+  ],
+  availabilityZones: ['us-west-2a', 'us-west-2b', 'us-west-2c'],
+  vpcCidrBlock: '10.238.0.0/19',
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
 //
-// const app = new ExtendedApp();
-// new DatabaseManagerStack(app, 'DatabaseManager', databaseManagerStackProps);
+const app = new ExtendedApp();
+new DatabaseManagerStack(app, 'DatabaseManager', databaseManagerStackProps);
